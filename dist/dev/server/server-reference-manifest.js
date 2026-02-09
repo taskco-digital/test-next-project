687e3f527855f138e03b36a6accc01c0623914d1
Md. Shahinur Islam <shahinur.educationhub@gmail.com>
2026-02-09 16:18:10 +0600

    section added

--- dist/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"k/MXwuU3+wQvdRKkndQHcsm+/Ta8vlFd322LPmtF1BE=\"\n}"
--- dist/dev/static/chunks/app_page_tsx_4c796de0._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/f27f0_next_dist_fd54bd33._.js"
-],
-    source: "dynamic"
-});
